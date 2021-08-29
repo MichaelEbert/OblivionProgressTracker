13b01ba49ea6c9788cea2e9e323c5e47c833691b
Michael Ebert <mebert@pacbell.net>
2021-08-29 02:16:06 -0700

    finished books.

--- data/books.js
+++ data/books.js
@@ -23,35 +23,110 @@
 ,{"id":19,"name":"Last Scabbard of Akrash","skill":"Armorer"}
 ,{"id":20,"name":"Light Armor Repair","skill":"Armorer"}
 
-,{"id":42,"name":"The Argonian Account, Book 1","skill":"Athletics"}
-,{"id":43,"name":"Beggar","skill":"Athletics"}
-,{"id":44,"name":"A Dance in Fire, v3","skill":"Athletics"}
-,{"id":45,"name":"The Ransom of Zarek","skill":"Athletics"}
-,{"id":46,"name":"The Red Kitchen Reader","skill":"Athletics"}
+,{"id":21,"name":"The Argonian Account, Book 1","skill":"Athletics"}
+,{"id":22,"name":"Beggar","skill":"Athletics"}
+,{"id":23,"name":"A Dance in Fire, v3","skill":"Athletics"}
+,{"id":24,"name":"The Ransom of Zarek","skill":"Athletics"}
+,{"id":25,"name":"The Red Kitchen Reader","skill":"Athletics"}
+       
+,{"id":26,"name":"2920, Morning Star (v1)","skill":"Blade"}
+,{"id":27,"name":"Battle of Sancre Tor","skill":"Blade"}
+,{"id":28,"name":"Fire and Darkness","skill":"Blade"}
+,{"id":29,"name":"Song Of Hrormir","skill":"Blade"}
+,{"id":30,"name":"Words and Philosophy","skill":"Blade"}
 
-,{"id":21,"name":"2920, Morning Star (v1)","skill":"Blade"}
-,{"id":22,"name":"Battle of Sancre Tor","skill":"Blade"}
-,{"id":23,"name":"Fire and Darkness","skill":"Blade"}
-,{"id":24,"name":"Song Of Hrormir","skill":"Blade"}
-,{"id":25,"name":"Words and Philosophy","skill":"Blade"}
+,{"id":31,"name":"A Dance in Fire, v2","skill":"Block"}
+,{"id":32,"name":"Death Blow of Abernanit","skill":"Block"}
+,{"id":33,"name":"The Mirror","skill":"Block"}
+,{"id":34,"name":"The Warp in the West","skill":"Block"}
+,{"id":35,"name":"Warrior","skill":"Block"}
 
-,{"id":26,"name":"A Dance in Fire, v2","skill":"Block"}
-,{"id":27,"name":"Death Blow of Abernanit","skill":"Block"}
-,{"id":28,"name":"The Mirror","skill":"Block"}
-,{"id":29,"name":"The Warp in the West","skill":"Block"}
-,{"id":30,"name":"Warrior","skill":"Block"}
+,{"id":36,"name":"The Importance of Where","skill":"Blunt"}
+,{"id":37,"name":"King","skill":"Blunt"}
+,{"id":38,"name":"The Legendary Sancre Tor","skill":"Blunt"}
+,{"id":39,"name":"Mace Etiquette","skill":"Blunt"}
+,{"id":40,"name":"Night Falls on Sentinel","skill":"Blunt"}
 
-,{"id":31,"name":"The Importance of Where","skill":"Blunt"}
-,{"id":32,"name":"King","skill":"Blunt"}
-,{"id":33,"name":"The Legendary Sancre Tor","skill":"Blunt"}
-,{"id":34,"name":"Mace Etiquette","skill":"Blunt"}
-,{"id":35,"name":"Night Falls on Sentinel","skill":"Blunt"}
+,{"id":41,"name":"2920, Frostfall (v10)","skill":"Conjuration"}
+,{"id":42,"name":"2920, Hearth Fire (v9)","skill":"Conjuration"}
+,{"id":43,"name":"The Doors of Oblivion","skill":"Conjuration"}
+,{"id":44,"name":"Liminal Bridges","skill":"Conjuration"}
+,{"id":45,"name":"Mythic Dawn Commentaries 1","skill":"Conjuration"}
+,{"id":46,"name":"The Warrior's Charge","skill":"Conjuration"}
 
-,{"id":36,"name":"2920, Frostfall (v10)","skill":"Conjuration"}
-,{"id":37,"name":"2920, Hearth Fire (v9)","skill":"Conjuration"}
-,{"id":38,"name":"The Doors of Oblivion","skill":"Conjuration"}
-,{"id":39,"name":"Liminal Bridges","skill":"Conjuration"}
-,{"id":40,"name":"Mythic Dawn Commentaries 1","skill":"Conjuration"}
-,{"id":41,"name":"The Warrior's Charge","skill":"Conjuration"}
+,{"id":47,"name":"The Art of War Magic","skill":"Destruction"}
+,{"id":48,"name":"The Horrors of Castle Xyr","skill":"Destruction"}
+,{"id":49,"name":"A Hypothetical Treachery","skill":"Destruction"}
+,{"id":50,"name":"Mystery of Talara, v 3","skill":"Destruction"}
+,{"id":51,"name":"Mythic Dawn Commentaries 2","skill":"Destruction"}
+,{"id":52,"name":"Response to Bero's Speech","skill":"Destruction"}
+
+,{"id":53,"name":"Ahzirr Traajijazeri","skill":"Hand to Hand"}
+,{"id":54,"name":"Immortal Blood","skill":"Hand to Hand"}
+,{"id":55,"name":"Master Zoaraym's Tale","skill":"Hand to Hand"}
+,{"id":56,"name":"Way of the Exposed Palm","skill":"Hand to Hand"}
+,{"id":57,"name":"The Wolf Queen, v 2","skill":"Hand to Hand"}
+
+,{"id":58,"name":"2920, MidYear (v6)","skill":"Heavy Armor"}
+,{"id":59,"name":"Chimarvamidium","skill":"Heavy Armor"}
+,{"id":60,"name":"Fighters Guild History, 1st Ed.","skill":"Heavy Armor"}
+,{"id":61,"name":"Hallgerd's Tale","skill":"Heavy Armor"}
+,{"id":62,"name":"History of the Fighters Guild","skill":"Heavy Armor"}
+,{"id":63,"name":"How Orsinium Passed to Orcs","skill":"Heavy Armor"}
+
+,{"id":64,"name":"The Argonian Account, Book 3","skill":"Illusion"}
+,{"id":65,"name":"Incident in Necrom","skill":"Illusion"}
+,{"id":66,"name":"Mystery of Talara, v 4","skill":"Illusion"}
+,{"id":67,"name":"Mythic Dawn Commentaries 3","skill":"Illusion"}
+,{"id":68,"name":"Palla, volume 1","skill":"Illusion"}
+,{"id":69,"name":"The Wolf Queen, v 3","skill":"Illusion"}
+
+,{"id":70,"name":"Ice and Chitin","skill":"Light Armor"}
+,{"id":71,"name":"Lord Jornibret's Last Dance","skill":"Light Armor"}
+,{"id":72,"name":"The Rear Guard","skill":"Light Armor"}
+,{"id":73,"name":"The Refugees","skill":"Light Armor"}
+,{"id":74,"name":"Rislav The Righteous","skill":"Light Armor"}
+
+,{"id":75,"name":"The Black Arrow, v 2","skill":"Marksman"}
+,{"id":76,"name":"A Dance in Fire, v5","skill":"Marksman"}
+,{"id":77,"name":"Father Of The Niben","skill":"Marksman"}
+,{"id":78,"name":"The Gold Ribbon of Merit","skill":"Marksman"}
+,{"id":79,"name":"Vernaccus and Bourlor","skill":"Marksman"}
+
+,{"id":80,"name":"2920, Sun's Height (v7)","skill":"Mercantile"}
+,{"id":81,"name":"The Buying Game","skill":"Mercantile"}
+,{"id":82,"name":"A Dance in Fire, v6","skill":"Mercantile"}
+,{"id":83,"name":"A Dance in Fire, v 7","skill":"Mercantile"}
+,{"id":84,"name":"The Wolf Queen, v 4","skill":"Mercantile"}
+
+,{"id":85,"name":"2920, Sun's Dawn (v2)","skill":"Mysticism"}
+,{"id":86,"name":"Before the Ages of Man","skill":"Mysticism"}
+,{"id":87,"name":"The Black Arts On Trial","skill":"Mysticism"}
+,{"id":88,"name":"The Firsthold Revolt","skill":"Mysticism"}
+,{"id":89,"name":"Mythic Dawn Commentaries 4","skill":"Mysticism"}
+,{"id":90,"name":"Souls, Black and White","skill":"Mysticism"}
+
+,{"id":91,"name":"2920, Rain's Hand (v4)","skill":"Restoration"}
+,{"id":92,"name":"The Exodus","skill":"Restoration"}
+,{"id":93,"name":"Mystery of Talara, v 2","skill":"Restoration"}
+,{"id":94,"name":"Notes on Racial Phylogeny","skill":"Restoration"}
+,{"id":95,"name":"Withershins","skill":"Restoration"}
+
+,{"id":96,"name":"Advances in Lock Picking","skill":"Security"}
+,{"id":97,"name":"The Locked Room","skill":"Security"}
+,{"id":98,"name":"Proper Lock Design","skill":"Security"}
+,{"id":99,"name":"Surfeit of Thieves","skill":"Security"}
+,{"id":100,"name":"The Wolf Queen, v 1","skill":"Security"}
+
+,{"id":101,"name":"2920, Last Seed (v8)","skill":"Sneak"}
+,{"id":102,"name":"Legend of Krately House","skill":"Sneak"}
+,{"id":103,"name":"Purloined Shadows","skill":"Sneak"}
+,{"id":104,"name":"Sacred Witness","skill":"Sneak"}
+,{"id":105,"name":"The Wolf Queen, v 6","skill":"Sneak"}
+
+,{"id":106,"name":"2920, Second Seed (v5)","skill":"Speechcraft"}
+,{"id":107,"name":"Biography of the Wolf Queen","skill":"Speechcraft"}
+,{"id":108,"name":"The Wolf Queen, v 5","skill":"Speechcraft"}
+,{"id":109,"name":"The Wolf Queen, v 7","skill":"Speechcraft"}
 ]}
 
