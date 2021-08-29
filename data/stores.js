66508139ccaffc31d11691407c6f524921c225e9
Michael Ebert <mebert@pacbell.net>
2021-08-28 22:17:26 -0700

    finished store list

--- data/stores.js
+++ data/stores.js
@@ -6,6 +6,101 @@
 ,{"id":4,"name":"Orrin","city":"Anvil"}
 ,{"id":5,"name":"Varel Morvayn","city":"Anvil"}
 ,{"id":6,"name":"Wilbur","city":"Anvil"}
+
 ,{"id":7,"name":"Ardaline","city":"Bravil"}
 ,{"id":8,"name":"Bogrum gro-Galash","city":"Bravil"}
+,{"id":9,"name":"Daenlin","city":"Bravil"}
+,{"id":10,"name":"Gilgondorin","city":"Bravil"}
+,{"id":11,"name":"Luciana Galena","city":"Bravil"}
+,{"id":12,"name":"Nilawen","city":"Bravil"}
+,{"id":13,"name":"Nordinor","city":"Bravil"}
+,{"id":14,"name":"Tadrose Helas","city":"Bravil"}
+
+,{"id":15,"name":"Fjotreid","city":"Bruma"}
+,{"id":16,"name":"Hafid Hollowleg","city":"Bruma"}
+,{"id":17,"name":"Karinnarre","city":"Bruma"}
+,{"id":18,"name":"Olav","city":"Bruma"}
+,{"id":19,"name":"Olfand","city":"Bruma"}
+,{"id":20,"name":"Ongar the World-Weary","city":"Bruma"}
+,{"id":21,"name":"Selena Orania","city":"Bruma"}
+,{"id":22,"name":"Sjkorta","city":"Bruma"}
+,{"id":23,"name":"Suurootan","city":"Bruma"}
+
+,{"id":24,"name":"Borba gra-Uzgash","city":"Cheydinhal"}
+,{"id":25,"name":"Dervera Romalen","city":"Cheydinhal"}
+,{"id":26,"name":"Eilonwy","city":"Cheydinhal"}
+,{"id":27,"name":"Mach-Na","city":"Cheydinhal"}
+,{"id":28,"name":"Magra gro-Naybek","city":"Cheydinhal"}
+,{"id":29,"name":"Mariana Ancharia","city":"Cheydinhal"}
+,{"id":30,"name":"Tertia Viducia","city":"Cheydinhal"}
+
+,{"id":31,"name":"Angalmo","city":"Chorrol"}
+,{"id":32,"name":"Emfrid","city":"Chorrol"}
+,{"id":33,"name":"Estelle Renoit","city":"Chorrol"}
+,{"id":34,"name":"Rasheda","city":"Chorrol"}
+,{"id":35,"name":"Sabine Laul","city":"Chorrol"}
+,{"id":36,"name":"Seed-Neeus","city":"Chorrol"}
+,{"id":37,"name":"Talasma","city":"Chorrol"}
+
+,{"id":38,"name":"Claudette Perrick","city":"IC — Market District"}
+,{"id":39,"name":"Delos Fandas","city":"IC — Market District"}
+,{"id":40,"name":"Hamlof Red-Tooth","city":"IC — Market District"}
+,{"id":41,"name":"Jensine","city":"IC — Market District"}
+,{"id":42,"name":"Maro Rufus","city":"IC — Market District"}
+,{"id":43,"name":"Ogier Georick","city":"IC — Market District"}
+,{"id":44,"name":"Palonirya","city":"IC — Market District"}
+,{"id":45,"name":"Phintias","city":"IC — Market District"}
+,{"id":46,"name":"Rindir","city":"IC — Market District"}
+,{"id":47,"name":"Rohssan","city":"IC — Market District"}
+,{"id":48,"name":"Sergius Verus","city":"IC — Market District"}
+,{"id":49,"name":"Tertullian Verus","city":"IC — Market District"}
+,{"id":50,"name":"Thoronir","city":"IC — Market District"}
+,{"id":51,"name":"Urbul gro-Orkulg","city":"IC — Market District"}
+,{"id":52,"name":"Varnado","city":"IC — Market District"}
+,{"id":53,"name":"Velus Hosidius","city":"IC — Market District"}
+,{"id":54,"name":"Viator Accius","city":"IC — Market District"}
+
+,{"id":55,"name":"Augusta Calidia","city":"IC — Other"}
+,{"id":56,"name":"Fathis Ules","city":"IC — Other"}
+,{"id":57,"name":"Julienne Fanis","city":"IC — Other"}
+,{"id":58,"name":"Ley Marillin","city":"IC — Other"}
+,{"id":59,"name":"Luther Broad","city":"IC — Other"}
+,{"id":60,"name":"Ormil","city":"IC — Other"}
+,{"id":61,"name":"Shady Sam","city":"IC — Other"}
+,{"id":62,"name":"Willet","city":"IC — Other"}
+
+,{"id":63,"name":"Batul gra-Sharob","city":"Kvatch"}
+,{"id":64,"name":"Sigrid","city":"Kvatch"}
+
+,{"id":65,"name":"Alves Uvenim","city":"Leyawiin"}
+,{"id":66,"name":"Bugak gro-Bol","city":"Leyawiin"}
+,{"id":67,"name":"Dar Jee","city":"Leyawiin"}
+,{"id":68,"name":"Elsynia","city":"Leyawiin"}
+,{"id":69,"name":"Gundalas","city":"Leyawiin"}
+,{"id":70,"name":"S'drassa","city":"Leyawiin"}
+,{"id":71,"name":"Shuravi","city":"Leyawiin"}
+,{"id":72,"name":"Tun-Zeeus","city":"Leyawiin"}
+,{"id":73,"name":"Witseidutsei","city":"Leyawiin"}
+
+,{"id":74,"name":"Agnete the Pickled","city":"Skingrad"}
+,{"id":75,"name":"Erina Jeranus","city":"Skingrad"}
+,{"id":76,"name":"Falanu Hlaalu","city":"Skingrad"}
+,{"id":77,"name":"Gunder","city":"Skingrad"}
+,{"id":78,"name":"Mog gra-Mogakh","city":"Skingrad"}
+,{"id":79,"name":"Sinderion","city":"Skingrad"}
+
+,{"id":80,"name":"Abhuki","city":"Other"}
+,{"id":81,"name":"Andreas Draconis","city":"Other"}
+,{"id":82,"name":"Candice Corgine","city":"Other"}
+,{"id":83,"name":"Christophe Marane","city":"Other"}
+,{"id":84,"name":"Davela Hlaren","city":"Other"}
+,{"id":85,"name":"Engorm","city":"Other"}
+,{"id":86,"name":"Etira Moslin","city":"Other"}
+,{"id":87,"name":"Foroch","city":"Other"}
+,{"id":88,"name":"Kirsten","city":"Other"}
+,{"id":89,"name":"Malene","city":"Other"}
+,{"id":90,"name":"Manheim Maulhand","city":"Other"}
+,{"id":91,"name":"Nerussa","city":"Other"}
+,{"id":92,"name":"S'thasa","city":"Other"}
+,{"id":93,"name":"Vlanhonder Moslin","city":"Other"}
 ]}
