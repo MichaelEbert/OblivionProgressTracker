9d16ab5a21b799abba470bcf6e65f9d60965c4ac
Michael Ebert <mebert@pacbell.net>
2021-08-28 21:41:32 -0700

    finished skill list

--- data/skills.js
+++ data/skills.js
@@ -7,5 +7,20 @@
 ,{"id":5,"name":"Blunt","specialization":"Combat"}
 ,{"id":6,"name":"Hand to Hand","specialization":"Combat"}
 ,{"id":7,"name":"Heavy Armor","specialization":"Combat"}
+
 ,{"id":8,"name":"Alchemy","specialization":"Magic"}
+,{"id":9,"name":"Alteration","specialization":"Magic"}
+,{"id":10,"name":"Conjuration","specialization":"Magic"}
+,{"id":11,"name":"Destruction","specialization":"Magic"}
+,{"id":12,"name":"Illusion","specialization":"Magic"}
+,{"id":13,"name":"Mysticism","specialization":"Magic"}
+,{"id":14,"name":"Restoration","specialization":"Magic"}
+
+,{"id":15,"name":"Acrobatics","specialization":"Stealth"}
+,{"id":16,"name":"Light Armor","specialization":"Stealth"}
+,{"id":17,"name":"Marksman","specialization":"Stealth"}
+,{"id":18,"name":"Mercantile","specialization":"Stealth"}
+,{"id":19,"name":"Security","specialization":"Stealth"}
+,{"id":20,"name":"Sneak","specialization":"Stealth"}
+,{"id":21,"name":"Speechcraft","specialization":"Stealth"}
 ]}
