5324876fded99864746a3a863507f8aee7b7ae3d
Michael Ebert <mebert@pacbell.net>
2021-09-26 00:39:34 -0700

    added sql script to create the tables. or something close enough to get the point across.

---
+++ server/db.sql
@@ -0,0 +1,12 @@
+-- these have not been vetted for accuracy. they are only what I remeber writing a week
+-- ago when I created the tables.
+CREATE TABLE urls(
+	[userkey] binary(64) NOT NULL PRIMARY KEY,
+	[url] char(6) NOT NULL UNIQUE
+)
+
+
+CREATE TABLE saves (
+    [url] char(6) NOT NULL PRIMARY KEY,
+	[saveData] varchar(4096) NULL
+)
