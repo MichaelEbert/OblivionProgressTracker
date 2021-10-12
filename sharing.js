355cee669336048d1130310f5d41ced5b820f032
Michael Ebert <mebert@pacbell.net>
2021-10-12 00:41:17 -0700

    Added share page for easier sharing.
    you can now click on a share page url and it'll load automatically, without you having to input share codes.
    tweaked the text of some of the settings on the settings page
    changed share url to give an actual url
    Created init function for share settings
    Added jsdoc to share functions
    made share functions async
    rejiggered share functions so you should just be able to call startSpectating(false) to update spectation, stopSpectating() to stop, and uploadCurrentSave() to update remote.

--- sharing.js
+++ sharing.js
@@ -2,6 +2,40 @@
 // sharing stuff
 // ==============
 
+/**
+ * checks to make sure that the global settings object has required properties for sharing.
+ * if not, set them to sensible defaults.
+ */
+function initShareSettings(){
+	let changed = false;
+	if(settings == null){
+		console.error("Sharing settings attempted to initalize before settings object exists");
+		return;
+	}
+
+	if(settings.shareKey == null){
+		settings.shareKey = generateSaveKey();
+		changed = true;
+	}
+	else if(settings.shareKey.length != 88){
+		console.error("share key '"+settings.shareKey+"' not correct length. Generating new share key.");
+		settings.shareKey = generateSaveKey();
+		changed = true;
+	}
+
+	if(settings.serverUrl == null || settings.serverUrl.length == 0){
+		settings.serverUrl = "https://ratskip.azurewebsites.net/share";
+		changed = true;
+	}
+
+	if(changed){
+		saveCookie("settings", settings);
+	}
+}
+
+/**
+ * @returns a new 64-byte, base64-encoded key that can be used for uploading data.
+ */
 function generateSaveKey(){
 	var typedArray = new Int8Array(64);
 	typedArray = window.crypto.getRandomValues(typedArray);
@@ -9,70 +43,104 @@ function generateSaveKey(){
 	return base64ArrayBuffer(typedArray);
 }
 
-function uploadSave(uploadUrl, saveData, myShareUrl, myShareKey){
-	let payload = {
-		saveData: JSON.stringify(saveData),
-		url: myShareUrl,
-		key: myShareKey
-	};
-	
-	var req = new XMLHttpRequest();
-    req.open("POST", uploadUrl, false);
-    req.setRequestHeader("Accept","application/json;odata=nometadata");
-    req.setRequestHeader("Content-Type","application/json");
-    req.send(JSON.stringify(payload));
-	
-	if(req.status == 200){
-		//yay.
-		return req.response;
-	}
-	return null;
+/**
+ * internal method to upload specified save data. 
+ * @param {string} uploadUrl URL endpoint to POST save data to.
+ * @param {object} saveData save data object to POST.
+ * @param {string} myShareCode share code to upload to.
+ * @param {string} myShareKey private 'password' for share code. Must be exactly 64 bytes, base64-encoded.
+ * @returns either resolve(response body) or reject(request)
+ */
+async function uploadSave(uploadUrl, saveData, myShareCode, myShareKey){
+	return new Promise((resolve, reject) =>{
+		let payload = {
+			saveData: JSON.stringify(saveData),
+			url: myShareCode,
+			key: myShareKey
+		};
+
+		var req = new XMLHttpRequest();
+		req.open("POST", uploadUrl, true);
+		req.setRequestHeader("Accept","application/json;odata=nometadata");
+		req.setRequestHeader("Content-Type","application/json");
+
+		req.onload = function () {
+			if(this.status == 200){
+				//yay.
+				resolve(this.response);
+			}
+		}
+
+		req.onerror = function (){
+			reject(this);
+		}
+		
+		req.send(JSON.stringify(payload));
+	});
 	//TODO: if 500 error, try agin later.
 }
 
-//download save.
-function downloadSave(remoteUrl){
-	var req = new XMLHttpRequest();
-    req.open("GET", remoteUrl, false);
-    req.setRequestHeader("Accept","application/json;odata=nometadata");
-    req.setRequestHeader("Content-Type","application/json");
-    req.send();
-	
-	if(req.status == 200){
-		//yay
-		return JSON.parse(req.response);
-	}
-	return null;	
+/**
+ * internal method to download save data from specified url.
+ * @param {string} remoteUrl url to download save data from
+ * @returns parsed json
+ */
+async function downloadSave(remoteUrl){
+	return new Promise((resolve, reject)=>{
+		var req = new XMLHttpRequest();
+		req.open("GET", remoteUrl, true);
+		req.setRequestHeader("Accept","application/json;odata=nometadata");
+		req.setRequestHeader("Content-Type","application/json");
+		
+		req.onload = function(){
+			if(req.status == 200){
+				//yay
+				resolve(JSON.parse(req.response));
+			}
+		}
+		req.onerror = function(){
+			reject(this);
+		}
+		req.send();
+	})
+		
 }
 
-//so we don't want remote save to replace current save.
-//so we just set "viewing remote" and disable saving.
-function uploadCurrentSave(){
-	if(settings.remoteShareUrl){
+/**
+ * Upload current save to server. If currently spectating, does nothing.
+ * we don't want remote save to replace current save. so we just set "viewing remote" and disable saving.
+ */
+async function uploadCurrentSave(){
+	if(settings.remoteShareCode){
 		//if we're viewing remote, don't upload.
 		console.log("viewing remote data, will not upload.");
 		return;
 	}
-	
+
+	initShareSettings();
 	let compressedData = compressSaveData(savedata);
-	if(settings.shareKey == null){
-		settings.shareKey = generateSaveKey();
-		saveCookie("settings",settings);
-	}
 	
-	var result = uploadSave(settings.serverUrl, compressedData, settings.myShareUrl, settings.shareKey);
-	
-	if(result){
-		settings.myShareUrl = result;
-		saveCookie("settings",settings);
-		const myShareUrlField = document.getElementById("myShareUrl");
-		myShareUrlField.value = result;
-	}
+	return uploadSave(settings.serverUrl, compressedData, settings.myShareCode, settings.shareKey)
+	.then((result)=>{
+		if(result){
+			if(settings.myShareCode != result){
+				console.log("my share code changed from '"+settings.myShareCode+"' to '"+result+"'");
+				settings.myShareCode = result;
+				saveCookie("settings",settings);
+			}
+			//do this every time we upload:
+			document.dispatchEvent(new Event("progressShared"));
+			alert("Uploaded");
+		}
+	});
 }
 
-function clearRemoteUrl(){
+/**
+ * stop spectating and go back to local save data.
+ */
+function stopSpectating(){
 	console.log("clearing remote data");
-	settings.remoteShareUrl = null;
+	settings.remoteShareCode = null;
 	saveCookie("settings",settings);
 	
 	var localProgress = loadCookie("progress_local");
@@ -81,29 +149,57 @@ function clearRemoteUrl(){
 	}
 	saveCookie("progress_local",{});
 	
-	loadProgressFromCookie();
+	//check for function before loading because /share.html spectates, but immediately redirects
+	// instead of updating progress.
+	if(loadProgressFromCookie){
+		loadProgressFromCookie();
+	}
 }
 
+/**
+ * Update data from spectating, or stop spectating if remote code is now blank.
+ * @param {boolean} notifyOnUpdate should we pop up dialog when we update
+ * @param {boolean} updateGlobalSaveData Should we decompress spectating data and store it in the global savedata variable?
+ */
+async function startSpectating(notifyOnUpdate = true, updateGlobalSaveData = true){
+	let code = settings.remoteShareCode;
+	if(code == null || code == ""){
+		stopSpectating();
+	}
+	else{
+		//TODO: move this if block. i dont think it belongs here.
+		if(Object.keys(loadCookie("progress_local")).length == 0){
+			//we don't have progress_local, so we can assume that the current value of 'progress' is local progress,
+			//so save it before we overwrite it with the remote data.
+			saveCookie("progress_local",loadCookie("progress"));
+		}
+
+		let downloadUrl = settings.serverUrl + "/" + settings.remoteShareCode; 
+		return downloadSave(downloadUrl)
+		.then((dl)=>{
+			if(dl){
+				saveCookie("progress",dl);
+				if(updateGlobalSaveData){
+					savedata = decompressSaveData(dl);
+				}
+				if(notifyOnUpdate){
+					alert("Downloaded");
+				}
+			}
+		});
+	}
+}
+
+/**
+ * Start (or stop) spectating by viewing a remote url.
+ * @param {Event} event onchange update.
+ */
 function setRemoteUrl(event){
-	if(event.target.value == ""){
-		clearRemoteUrl();
-		return;
-	}
-	if(Object.keys(loadCookie("progress_local")).length == 0){
-		//we don't have local progress, so we can assume that current progress is local.
-		saveCookie("progress_local",compressSaveData(savedata));
-	}
-	
-	settings.remoteShareUrl = event.target.value;
-	saveCookie("settings",settings);
-	
-	let downloadUrl = settings.serverUrl + "/" + settings.remoteShareUrl; 
-	let dl = downloadSave(downloadUrl);
-	
-	if(dl){
-		savedata = decompressSaveData(dl);
-		saveCookie("progress",dl);
-		alert("Downloaded");
+	{
+		initShareSettings();
+		settings.remoteShareCode = event.target.value;
+		saveCookie("settings",settings);
+		startSpectating();
 	}
 }
 
