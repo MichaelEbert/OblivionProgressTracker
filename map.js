f2d0e18ef9a4c2fb649fec2d35bd2b282fbddc82
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2021-11-06 01:53:42 -0400

    added mouseover icon calculations

--- map.js
+++ map.js
@@ -63,6 +63,8 @@ function initMap(){
     wrapper.onmouseout = function(){mousedown = false;};
     wrapper.onmousemove = function(e){
         if(mousedown){moveMap(e);}
+        
+        //Overlay mouseover
         if(e.offsetY >= 10  && e.offsetY <= 20){
             var x = document.getElementById("wrapper_Map").style.width.slice(0,document.getElementById("wrapper_Map").style.width.length-2);
             if(e.offsetX >= 8 && e.offsetX <= x/3 - 1){
@@ -88,12 +90,26 @@ function initMap(){
             if(redraw){
                 drawOverlay();
             }
+            //End Overlay mouseover
 
-            if(locArr){
+            //mouseover icon
+            
+
+            if(locArr && !mousedown && currentOverlay == "Locations"){
                 locArr.forEach(element => {
-                    
+                    let cCords = worldSpaceToCanvasSpace(element.x, element.y);
+
+                    if(cCords.x < e.offsetX &&
+                        cCords.x + cCords.wh > e.offsetX &&
+                        cCords.y < e.offsetY &&
+                        cCords.y + cCords.wh > e.offsetY){
+                        console.log("yes, " + element.name);
+                        console.log(cCords.x + '' + cCords.y)
+                    }
+
                 });
             }
+            //End mouseover icon
         }
     };
     wrapper.onwheel = function(e){    
@@ -117,47 +133,34 @@ function drawMap(){
     mapCanvasContext.drawImage(img_Map, mapX, mapY, (img_Map.width * zoomLevel), (img_Map.height * zoomLevel), 
                                     0, 0, img_Map.width, img_Map.height);
 
-    //draw all map icons //TODO: add in overlay options
     if(currentOverlay == "Locations"){
         for(let i = 0; i < locArr.length;i++){
-            if(locArr[i].icon == "Ayleid") drawIcon(icons.Ayleid, locArr[i].X, locArr[i].Y, locArr[i].name);
-            else if(locArr[i].icon == "Camp") drawIcon(icons.Camp, locArr[i].X, locArr[i].Y, locArr[i].name);
-            else if(locArr[i].icon == "Cave") drawIcon(icons.Cave, locArr[i].X, locArr[i].Y, locArr[i].name);
-            else if(locArr[i].icon == "Fort") drawIcon(icons.Fort, locArr[i].X, locArr[i].Y, locArr[i].name);
-            else if(locArr[i].icon == "Gate") drawIcon(icons.Gate, locArr[i].X, locArr[i].Y, locArr[i].name);
-            else if(locArr[i].icon == "Inn") drawIcon(icons.Inn, locArr[i].X, locArr[i].Y, locArr[i].name);
-            else if(locArr[i].icon == "Landmark") drawIcon(icons.Landmark, locArr[i].X, locArr[i].Y, locArr[i].name);
-            else if(locArr[i].icon == "Mine") drawIcon(icons.Mine, locArr[i].X, locArr[i].Y, locArr[i].name);
-            else if(locArr[i].icon == "Settlement") drawIcon(icons.Settlement, locArr[i].X, locArr[i].Y, locArr[i].name);
-            else if(locArr[i].icon == "Shrine") drawIcon(icons.Shrine, locArr[i].X, locArr[i].Y, locArr[i].name);
-            else console.warn("Element at " + i +" has no icon."); //catch all incase something changes.
-
-            //check if visited and add a check/feedback thing for each place visited.
-            // if(visited){
-            //     drawIcon(icons.Check, locArr[i].X, locArr[i].Y);
-            // }
+            if(locArr[i].icon == "Ayleid") drawIcon(icons.Ayleid, locArr[i].x, locArr[i].y, locArr[i].name);
+            else if(locArr[i].icon == "Camp") drawIcon(icons.Camp, locArr[i].x, locArr[i].y, locArr[i].name);
+            else if(locArr[i].icon == "Cave") drawIcon(icons.Cave, locArr[i].x, locArr[i].y, locArr[i].name);
+            else if(locArr[i].icon == "Fort") drawIcon(icons.Fort, locArr[i].x, locArr[i].y, locArr[i].name);
+            else if(locArr[i].icon == "Gate") drawIcon(icons.Gate, locArr[i].x, locArr[i].y, locArr[i].name);
+            else if(locArr[i].icon == "Inn") drawIcon(icons.Inn, locArr[i].x, locArr[i].y, locArr[i].name);
+            else if(locArr[i].icon == "Landmark") drawIcon(icons.Landmark, locArr[i].x, locArr[i].y, locArr[i].name);
+            else if(locArr[i].icon == "Mine") drawIcon(icons.Mine, locArr[i].x, locArr[i].y, locArr[i].name);
+            else if(locArr[i].icon == "Settlement") drawIcon(icons.Settlement, locArr[i].x, locArr[i].y, locArr[i].name);
+            else if(locArr[i].icon == "Shrine") drawIcon(icons.Shrine, locArr[i].x, locArr[i].y, locArr[i].name);
+            else console.warn("Element at " + i +" has no icon."); //catch all in case something changes.
 
             //figure out where to draw names or check for mouse over icon and draw it's name.
-            //
             mapCanvasContext.beginPath();
             mapCanvasContext.fillStyle = "black";
             mapCanvasContext.textAlign = "center";
             mapCanvasContext.font = "16px Arial";
-            mapCanvasContext.fillText(locArr[i].name, locArr[i].X, locArr[i].Y);
+            mapCanvasContext.fillText(locArr[i].name, locArr[i].x, locArr[i].y);
             mapCanvasContext.fill();
         }
     }
     if(currentOverlay == "NirnRoute"){
-        
         for(let i = 0; i < nirnArr.length;i++){
             if(nirnArr[i].cell == "Outdoors"){
                 drawIcon(icons.Camp, Math.round(nirnArr[i].x), Math.round(nirnArr[i].y));
             }
-            
-            //check if visited and add a check/feedback thing for each place visited.
-            // if(visited){
-            //     drawIcon(icons.Check, locArr[i].X, locArr[i].Y);
-            // }
         }
     }
     if(currentOverlay == "Exploration"){
@@ -187,35 +190,13 @@ function drawMap(){
 
 //give x/y as reg in game cords.
 function drawIcon(icon, iconX = 0.5, iconY = 0.5){
-    var MapW = img_Map.width;
-    var MapH = img_Map.height;
+    
+    var canvasCords = worldSpaceToCanvasSpace(iconX, iconY);
 
-    //scale icons by zoom
-    var iconWH = 20 / zoomLevel;
-    if(zoomLevel > 1.25){
-        iconWH = 20 / zoomLevel * 1.25;
-    }
-    if(zoomLevel > 1.5){
-        iconWH = 20 / zoomLevel * 1.5;
-    }
-    if(zoomLevel > 1.75){
-        iconWH = 20 / zoomLevel * 2;
-    }
-
-    var worldW = 480000;
-    var worldH = 400000;
-
-    //convert worldspace loc to % loc
-    iconX = (iconX + worldW / 2) / worldW;
-    iconY = (-iconY + worldH / 2) / worldH;
-
-    //adjust % loc to account for "camera" position
-    var x = ((MapW * iconX) - mapX) / zoomLevel - iconWH;
-    var y = ((MapH * iconY) - mapY) / zoomLevel - iconWH;
-    mapCanvasContext.drawImage(icon, x, y, iconWH, iconWH);
+    mapCanvasContext.drawImage(icon, canvasCords.x, canvasCords.y, canvasCords.wh, canvasCords.wh);
     
     if(discovered){
-        mapCanvasContext.drawImage(icons.Check, x, y, iconWH, iconWH);
+        mapCanvasContext.drawImage(icons.Check, canvasCords.x, canvasCords.y, canvasCords.wh, canvasCords.wh);
     }
 }
 
@@ -341,4 +322,32 @@ function initImgs(){
         icons[i].src = "images/Icon_" + i + ".png";
         }
     )
+}
+
+//converts worldspace cords into relative canvas cords.
+//returns object{x,y} for canvas space.
+function worldSpaceToCanvasSpace(x = 0, y = 0){
+    var MapW = img_Map.width;
+    var MapH = img_Map.height;
+
+    var worldW = 480000;
+    var worldH = 400000;
+
+    x = (x + worldW / 2) / worldW;
+    y = (-y + worldH / 2) / worldH;
+
+    var wh = 20 / zoomLevel;
+    if(zoomLevel > 1.25){
+        wh = 20 / zoomLevel * 1.25;
+    }
+    if(zoomLevel > 1.5){
+        wh = 20 / zoomLevel * 1.5;
+    }
+    if(zoomLevel > 1.75){
+        wh = 20 / zoomLevel * 2;
+    }
+
+    var x = ((MapW * x) - mapX) / zoomLevel - wh;
+    var y = ((MapH * y) - mapY) / zoomLevel - wh;
+    return {x:x, y:y, wh:wh}
 }
