dbdaab39f38f57eb540164c1d18a4358e4a990d1
Michael Ebert <mebert@pacbell.net>
2022-04-16 00:16:38 -0700

    Added reset button to settings page. Fixed reset button on main page. Created special role for autobot user so in the future it can't access ips table. Added accessed column to saves table. Currently writes update it, but not reads.

--- server/user.sql
+++ server/user.sql
@@ -1,3 +1,2 @@
 --CREATE USER autobot WITH PASSWORD='aaaaa'
---exec sp_addrolemember 'db_datareader', 'autobot'
---exec sp_addrolemember 'db_datawriter','autobot'
+ALTER ROLE normalReaderWriter ADD MEMBER autobot;
