b9f3040e0a606960a4159df87846b7db99849c7e
Michael Ebert <mebert@pacbell.net>
2022-12-11 22:45:12 -0800

    fixed replacing wrong string in replace.ps1

--- template/replace.ps1
+++ template/replace.ps1
@@ -10,16 +10,18 @@ Param(
 	[string]$endKey,
 	[System.IO.FileSystemInfo]$replacementFile)
 Begin{
-	$replacementText = [System.IO.File]::ReadAllText($replacementFile.fullname);
-	if(!$replacementText.startsWith($beginKey) -or !$replacementText.endsWith($endKey)){
-		throw "replacement $replacementFile must begin with $beginKey and end with $endKey!"
+	$replacementTextBase = [System.IO.File]::ReadAllText($replacementFile.fullname);
+	if(!$replacementTextBase.startsWith($beginKey)){
+		throw "replacement $replacementFile must begin with $beginKey"
+	}
+	if(!$replacementTextBase.endsWith($endKey)){
+		throw "replacement $replacementFile must end with $endKey"
 	}
 	write-verbose "==Replacing $replacementFile=="
-	$fileContents = [System.IO.File]::ReadAllText($file.fullname,[System.Text.Encoding]::UTF8);
 }
 Process{
 	foreach($file in $files){
-		$contents = $fileContents
+		$contents = [System.IO.File]::ReadAllText($file.fullname,[System.Text.Encoding]::UTF8);
 		$relPath = resolve-path -relative $file.fullname;
 		if(!$contents.contains($beginKey)){
 			write-verbose "skipping $relPath as it does not contain $beginKey" 
@@ -30,9 +32,10 @@ Process{
 			continue;
 		}
 		#correctly replace in tools folder, etc.
+		$replacementText = $replacementTextBase
 		#TODO make generic
 		if($relPath.startsWith("..\tools")){
-			$contents = [Regex]::replace($contents,"href=`"\./","href=`"../");
+			$replacementText = [Regex]::replace($replacementTextBase,"href=`"\./","href=`"../");
 		}
 
 		write-verbose "Replacing in $relPath" 
@@ -43,8 +46,6 @@ Process{
 }
 }
 
-
-$replacementText = [System.IO.File]::ReadAllText((get-item navbar.html).fullname);
 get-childitem "../" "*.html" -recurse | replaceText -beginKey '<!-- begin topbar-->' -endKey '<!-- end topbar-->' -replacementFile (get-item navbar.html)
 get-childitem "../" "*.html" -recurse | replaceText -beginKey '<!-- BEGIN FOOTER-->' -endKey '<!-- END FOOTER-->' -replacementFile (get-item footer.html)
 
