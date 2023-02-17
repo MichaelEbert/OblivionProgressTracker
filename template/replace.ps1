eff35260782c064deb90fd12964c8506bd39489d
Michael Ebert <mebert@pacbell.net>
2023-02-17 11:05:13 -0800

    replace script fix

--- template/replace.ps1
+++ template/replace.ps1
@@ -36,6 +36,7 @@ Process{
 		#TODO make generic
 		if($relPath.startsWith("..\tools")){
 			$replacementText = [Regex]::replace($replacementTextBase,"href=`"\./","href=`"../");
+			$replacementText = [Regex]::replace($replacementTextBase,"src=`"\./","src=`"../");
 		}
 
 		write-verbose "Replacing in $relPath" 
