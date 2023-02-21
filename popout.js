b45a03a92b620aeab06cf89001b7ac1a80477b1e
MeemawHustlin <76759777+MeemawHustlin@users.noreply.github.com>
2023-02-20 21:30:08 -0800

    set up window params for modifying popout
    - columns don't do anything yet. Not sure how to implement this currently.
    - User settings do properly pass to window params and they are read properly, but certain settings seem to not work if they are too small of values. Need to investigate and possibly redo a lot of the css here to make things more automatically formatted.

---
+++ popout.js
@@ -0,0 +1,28 @@
+//Runs in the settings.html page when the user clicks the "Generate Browser Source Tracker" button. Generates a popout window and passes params.
+function initBrowserSource(){
+    //default settings
+    var width = 460;
+    var height = 330;
+    var columns = 2;
+
+    //Override settings with user preferences if applicable
+    width = document.getElementById("browserSourceWidth").value ?? 0;
+    height = document.getElementById("browserSourceHeight").value ?? 0;
+    columns = document.getElementById("browserSourceColumns").value ?? 0;
+
+    //generate the popout window based on settings.
+    var windowParamsURL = 'popout.html?width=' + width + 'px&height=' + height + 'px&columns=' + columns;
+    var windowFeaturesStr = 'toolbar=no,menubar=no,width=' + width + ',height=' + height;
+    window.open(windowParamsURL, 'browserSource', windowFeaturesStr);
+}
+
+//Runs in the popout.html page. Takes the window parameters generated in the previous function and modifies html elements to fit these parameters.
+function initPopoutSettings(){
+    //get window parameters from url.
+    const windowParams = window.location.search;
+    const urlParams = new URLSearchParams(windowParams);
+
+    //update popout html elements based on settings.
+    window.getElementById(popoutContainer).style.width = urlParams.get('width');
+    window.getElementById(popoutContainer).style.height = urlParams.get('height');
+}
