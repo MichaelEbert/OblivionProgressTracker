df1ebc9c2a94940610dbbc6aa267966963244883
Michael Ebert <mebert@pacbell.net>
2023-03-14 17:56:54 -0700

    updated to dotnet 6. added live view count at /url/{url}/views.

---
+++ server/ViewCountHandler.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShareApi
+{
+    [Route("url/{url}/views")]
+    [ApiController]
+    public class ViewCountHandler : ControllerBase
+    {
+        public static ViewCount ViewCounter = new ViewCount();
+
+        [HttpGet]
+        public ActionResult<int> HandleViewCount(string url)
+        {
+            return Ok(ViewCounter.GetViewCount(url));
+        }
+    }
+}
